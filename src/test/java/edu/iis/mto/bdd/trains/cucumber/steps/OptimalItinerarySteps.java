135620048f457e5bc17865f29b4f0e20ec3f90b7
krecikovsky112 <krecikovsky112@gmail.com>
2021-05-31 19:27:57 +0200

    feature1_impl: Change in OptimalItinerarySteps

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -1,19 +1,15 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import java.util.List;
-
-import edu.iis.mto.bdd.trains.model.Line;
+import edu.iis.mto.bdd.trains.services.BasicIntineraryService;
 import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
 import edu.iis.mto.bdd.trains.services.IntineraryService;
-import edu.iis.mto.bdd.trains.services.TimetableService;
-import org.hamcrest.Matchers;
 import org.joda.time.LocalTime;
-
-import cucumber.api.PendingException;
 import cucumber.api.Transform;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import static org.hamcrest.Matchers.is;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -25,17 +21,16 @@ public class OptimalItinerarySteps {
     @Given("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
-        throw new PendingException();
-
+        intineraryService = new BasicIntineraryService(new InMemoryTimetableService(), 15);
     }
 
     @When("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
     public void whenIWantToTravel(String departure, String destination, @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
-        this.arrivalTimes = intineraryService.findNextDepartures(departure, destination, startTime);
+        arrivalTimes = intineraryService.findNextDepartures(departure, destination, startTime);
     }
 
     @Then("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-        assertThat(arrivalTimes, Matchers.is(expectedTrainTimes));
+        assertThat(arrivalTimes, is(expectedTrainTimes));
     }
 }
